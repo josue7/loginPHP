e571f0e60278aef27cd8eb6b5d9709323597a223
Josue Marfil <josue7@users.noreply.github.com>
2023-04-17 19:39:22 -0600

    Variables de conexión
    Se define las constantes que se utilizan para conectarse a la base de datos, estas se agregan a la clase Database.

---
+++ connection/datos.php
@@ -0,0 +1,12 @@
+<?php
+    /** Desarrollado por Josué Marfil
+     * @version 1.0
+     * email: josue.marfilg@gmail.com
+     * @license http://creativecommons.org/licenses/by-nc-sa/3.0/
+     */
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'josh');
+    define('DB_NAME', 'loginphp');
+?>
